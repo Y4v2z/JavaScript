485eb3b8c80e7d223bca47ecb60cc5d579d6315e
Yavuz AVCİ <yavuzavci01@gmail.com>
2022-10-17 15:23:51 +0300

    11. gün eklendi

--- 10.gün/script.js
+++ 10.gün/script.js
@@ -22,6 +22,7 @@
 
 // console.log(val);
 
+
 // *******************************************************
 
 
@@ -116,59 +117,59 @@
 
 
 // Traversin the Dom (elementler üzerinde gezinme)
-let val;
-let list = document.querySelector(".list-group");
-val = list;
-val = list.childNodes;
+// let val;
+// let list = document.querySelector(".list-group");
+// val = list;
+// val = list.childNodes;
 
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[0].nodeType; //text node
-val = list.childNodes[1].nodeType; //element
-
-
-val = list.children;
-val = list.children[0];
-val = list.children[2];
-val = list.children[2].textContent = ("new item");
-val = list.children[3].children;
-val = list.children[3].children[0];
-
-val = list.firstChild;
-val = list.firstElementChild;
-
-val = list.lastChild;
-val = list.lastElementChild;
-
-val = list.childElementCount;
-val = list.parentNode;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[0].nodeType; //text node
+// val = list.childNodes[1].nodeType; //element
 
 
-val = list.parentElement;
-val = list.parentElement.parentElement;
+// val = list.children;
+// val = list.children[0];
+// val = list.children[2];
+// val = list.children[2].textContent = ("new item");
+// val = list.children[3].children;
+// val = list.children[3].children[0];
+
+// val = list.firstChild;
+// val = list.firstElementChild;
+
+// val = list.lastChild;
+// val = list.lastElementChild;
+
+// val = list.childElementCount;
+// val = list.parentNode;
 
 
-val = list.children[0].nextSibling;
-val = list.children[0].nextElementSibling;
+// val = list.parentElement;
+// val = list.parentElement.parentElement;
 
-val = list.children[1].previousSibling;
-val = list.children[1].previousElementSibling;
 
-console.log(val);
+// val = list.children[0].nextSibling;
+// val = list.children[0].nextElementSibling;
 
-// for (let i = 0; i < list.children.length; i++) {
-//     console.log(list.children[i]);
-// }
+// val = list.children[1].previousSibling;
+// val = list.children[1].previousElementSibling;
+
+// console.log(val);
+
+// // for (let i = 0; i < list.children.length; i++) {
+// //     console.log(list.children[i]);
+// // }
+
+// // for (let i = 0; i < list.childNodes.length; i++) {
+// //     console.log(list.childNodes[i]);
+// // }
 
 // for (let i = 0; i < list.childNodes.length; i++) {
-//     console.log(list.childNodes[i]);
+//     if (list.childNodes[i].nodeType === 1)
+
+//         console.log(list.childNodes[i]);
 // }
-
-for (let i = 0; i < list.childNodes.length; i++) {
-    if (list.childNodes[i].nodeType === 1)
-
-        console.log(list.childNodes[i]);
-}
 
 
 
